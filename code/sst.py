f3fccfb2c7f525f612730f0fd86b0a26124667d4
Joseph Zhong <josephz@cs.washington.edu>
2019-01-15 17:00:34 -0800

    fix sst: implements identity

--- code/sst.py
+++ code/sst.py
@@ -3,7 +3,13 @@ import conversion
 import console
 
 def global_eq_match(content, style):
-    return content
+    """
+
+    :param content:
+    :param style:
+    """
+
+    return style
 
 def stylize(content, style):
     stylized = global_eq_match(content, style)
@@ -12,14 +18,25 @@ def stylize(content, style):
 def main():
     style_path = "sample/style.mp3"
     content_path = "sample/content.mp3"
-    stylized_path = "sample/stylized.mp3"
+    stylized_img_path = "sample/stylized.jpg"
+    stylized_audio_path = "sample/stylized.mp3"
 
-    style_img = conversion.file_to_image(style_path)
-    content_img = conversion.file_to_image(content_path)
-    stylized = stylize(content_img, style_img)
+    # Read style audio to spectrograms.
+    style_audio, style_sample_rate = conversion.file_to_audio(style_path)
+    style_img, style_phase = conversion.audio_to_spectrogram(style_audio, fft_window_size=1536)
 
-    conversion.image_to_file(stylized, stylized_path)
-    console.log("done! saved to", stylized_path)
+    # Read content audio to spectrograms.
+    content_audio, content_sample_rate = conversion.file_to_audio(content_path)
+    content_img, content_phase = conversion.audio_to_spectrogram(content_audio, fft_window_size=1536)
+
+    import pdb; pdb.set_trace()
+    stylized_img = stylize(content_img, style_img)
+    stylized_audio = conversion.amplitude_to_audio(stylized_img, 1536)
+
+    # Save stylized spectrogram and audio.
+    conversion.image_to_file(stylized_img, stylized_img_path)
+    conversion.audio_to_file(stylized_audio, stylized_audio_path)
+    console.log("done! saved to sample")
 
 if __name__ == "__main__":
     main()
