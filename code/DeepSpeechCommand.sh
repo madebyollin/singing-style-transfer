b314d4789d06d833c8978ad34323c705352d1d1a
Ollin Boer Bohan <madebyollin@gmail.com>
2019-02-03 20:00:40 -0800

    proper handling of file paths with spaces in DSC.sh

--- code/DeepSpeechCommand.sh
+++ code/DeepSpeechCommand.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sox ${1} /tmp/input.wav remix 1,2
+sox "${1}" /tmp/input.wav remix 1,2
 python ../extern/DeepSpeech/DeepSpeech-inference-hack.py --one_shot_infer="/tmp/input.wav" --checkpoint_dir="../extern/DeepSpeech/deepspeech-0.4.1-checkpoint/" --np_output_path="${2}" --alphabet_config_path="../extern/DeepSpeech/data/alphabet.txt"
