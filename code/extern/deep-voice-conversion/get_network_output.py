403169e366fb8e365d0b78f411efd59d91ad0171
Joseph Zhong <josephz@cs.washington.edu>
2019-03-05 20:18:31 -0800

    code/extern/deep-voice-conversion: add stitched heatmap

--- code/extern/deep-voice-conversion/get_network_output.py
+++ code/extern/deep-voice-conversion/get_network_output.py
@@ -41,7 +41,7 @@ def init_predictor(ckpt_dir):
     predictor = OfflinePredictor(pred_conf)
     return predictor
 
-def get_network_output2(wav, ckpt_dir=CKPT_DIR, out_path_fmt="outputs/test2_{:04d}_{:04d}.png"):
+def get_network_output(wav, ckpt_dir=CKPT_DIR, out_path_fmt="outputs/test2_{:04d}_{:04d}.png"):
     """ Computes PPGs for a given loaded wav audio.
     This splits the input wav file into two-second batches and runs each through the Phoneme classifier.
     For each batch, this outputs each ppg to the given output_path.
@@ -75,6 +75,7 @@ def get_network_output2(wav, ckpt_dir=CKPT_DIR, out_path_fmt="outputs/test2_{:04
     ppgs = preds[0]
 
     # Output each ppg.
+    heatmaps = []
     for i, heatmap in enumerate(ppgs):
         assert 0 <= np.min(heatmap) <= np.max(heatmap) <= 1.0
         out_path = out_path_fmt.format(i, num_splits-1)
@@ -93,14 +94,32 @@ def get_network_output2(wav, ckpt_dir=CKPT_DIR, out_path_fmt="outputs/test2_{:04
         heatmap = 255 * heatmap
         im = Image.fromarray(heatmap.astype(np.uint8))
 
-        # Save the image.
+        # Save the image to disk.
         im.save(out_path)
 
+        # Accumulate heatmap.
+        heatmaps.append(heatmap)
+    return heatmaps
+
+def get_heatmap(wav):
+    # Get non-stitched and un-upsampled heatmaps.
+    heatmaps = get_network_output(wav)
+
+    # Stitch and upsample heatmaps into one final heatmap.
+    stitched_heatmap = np.concatenate(heatmaps, axis=1)
+    return stitched_heatmap
+
+
 if __name__ == "__main__":
+    # REVIEW josephz: This should really be a cmd_line parameter.
     wav_file = "test_data/reference_stylized.wav"
     assert os.path.isfile(wav_file)
     wav, _ = librosa.load(wav_file, sr=hp.default.sr)
-    get_network_output2(wav)
+
+    stitched_heatmap = get_heatmap(wav)
+    im = Image.fromarray(stitched_heatmap.astype(np.uint8))
+    im.save("outputs/heatmap.png")
+
 
 
 
